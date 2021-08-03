03349e402bebd07dae54d954b606a3c53cbfe219
Iskander Khisamov <iskanderkhisamov@gmail.com>
2021-08-03 14:57:05 +0300

    added React, Spring

---
+++ server/src/test/java/studio/khisamov/traxel/TraxelApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.khisamov.traxel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraxelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
